791c58bcec3574b787708bc215b4f19c3973eeae
Ion Petras <petrasanton027@gmail.com>
2020-06-03 18:28:24 +0100

    Appointment class updated, updated fields

--- appointmentScheduler/src/main/java/com/appointment/model/Appointment.java
+++ appointmentScheduler/src/main/java/com/appointment/model/Appointment.java
@@ -1,5 +1,44 @@
 package com.appointment.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Appointment {
+    private long id;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private List<User> appointmentAttendee;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setAppointmentAttendee(List<User> appointmentAttendee) {
+        this.appointmentAttendee = appointmentAttendee;
+    }
+
+    public List<User> getAppointmentAttendee() {
+        return appointmentAttendee;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
 
 }
