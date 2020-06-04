702f6061c784c66e17be8b100dbab502aa987ffa
Ion Petras <petrasanton027@gmail.com>
2020-06-04 09:24:25 +0100

    empty line removed

---
+++ appointmentScheduler/src/main/java/com/appointment/dao/UserDAO.java
@@ -0,0 +1,5 @@
+package com.appointment.dao;
+
+public class UserDAO {
+    
+}
--- appointmentScheduler/src/main/java/com/appointment/model/Appointment.java
+++ appointmentScheduler/src/main/java/com/appointment/model/Appointment.java
@@ -40,5 +40,4 @@ public class Appointment {
     public LocalDateTime getStartTime() {
         return startTime;
     }
-
 }
