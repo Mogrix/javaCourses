6ba6e6e1261728b839d4adcc9b65da1224c2553c
Ion Petras <petrasanton027@gmail.com>
2020-06-08 13:17:42 +0100

    CRUD methods created

--- appointmentScheduler/src/main/java/com/appointment/dao/UserDAO.java
+++ appointmentScheduler/src/main/java/com/appointment/dao/UserDAO.java
@@ -1,5 +1,21 @@
 package com.appointment.dao;
 
+import com.appointment.model.User;
+
 public class UserDAO {
-    
+    public long create(User user){
+     return 5;
+    }
+
+    public User findByID(long id ){
+        return new User();
+    }
+
+    public void update(User user){
+
+    }
+
+    public void delete(long id){
+
+    }
 }
